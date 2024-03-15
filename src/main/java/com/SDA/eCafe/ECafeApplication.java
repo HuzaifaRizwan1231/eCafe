02b771c0e5c976b2fec44599c929459c968fd7f3
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-03-15 12:42:13 +0500

    initialized project

---
+++ src/main/java/com/SDA/eCafe/ECafeApplication.java
@@ -0,0 +1,13 @@
+package com.SDA.eCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECafeApplication.class, args);
+	}
+
+}
