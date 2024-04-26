be67c84d9001f10adb77fc0b6f536782a3c81fbe
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-04-27 03:03:46 +0500

    Added product details functionality

--- src/main/java/com/SDA/eCafe/controller/ProductController.java
+++ src/main/java/com/SDA/eCafe/controller/ProductController.java
@@ -2,16 +2,21 @@ package com.SDA.eCafe.controller;
 
 import com.SDA.eCafe.model.Product;
 import com.SDA.eCafe.repository.ProductRepository;
+import com.SDA.eCafe.service.ProductService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 //@RestController
 @Controller
@@ -25,6 +30,9 @@ public class ProductController {
         this.productRepository = productRepository;
     }
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/")
     public String getAllProducts(Model model) {
         try {
@@ -36,4 +44,19 @@ public class ProductController {
             return "error";
         }
     }
+
+    @GetMapping("/productdetails/{id}")
+    public String getProductDetail(@PathVariable("id") int productId, Model model) {
+        try {
+            Product product = productService.getProductById(productId);
+            System.out.println(product);
+            model.addAttribute("product", product);
+            return "productDetails";
+        }catch(Exception error){
+            return "error";
+        }
+    }
+
+    
+
 }
