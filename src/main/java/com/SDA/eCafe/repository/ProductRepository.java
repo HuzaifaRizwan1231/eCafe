080d3f8278055d2031e7a68acee52f429c8ff400
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-05-02 00:33:49 +0500

    Created analytics page for admin

--- src/main/java/com/SDA/eCafe/repository/ProductRepository.java
+++ src/main/java/com/SDA/eCafe/repository/ProductRepository.java
@@ -16,5 +16,8 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     @Query("SELECT p FROM Product p WHERE LOWER(p.Name) LIKE %:keyword% OR LOWER(p.Description) LIKE %:keyword%")
     List<Product> findProductsByKeywordIgnoreCase(@Param("keyword") String keyword);
+
+    @Query("SELECT p.Name, SUM(o.Quantity*o.ProductPrice) AS TotalSale  FROM Orders o JOIN Product p ON p.ID = o.ProductId WHERE o.Status = 'Completed' GROUP BY o.ProductId")
+    List<Object[]> getAllProductsWithSale();
 }
 
