cb08cbda5dfc2e4b3c51c4ceb32a558d4a029916
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-05-01 21:15:16 +0500

    added total price for place order

--- src/main/java/com/SDA/eCafe/controller/UserController.java
+++ src/main/java/com/SDA/eCafe/controller/UserController.java
@@ -1,3 +1,5 @@
+package com.SDA.eCafe.controller;
+
 import com.SDA.eCafe.model.User;
 import com.SDA.eCafe.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,8 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -69,6 +77,7 @@ public class UserController {
             }
         } catch (Exception e) {
             e.printStackTrace();
+            return "error";
         }
     }
   
@@ -93,7 +102,7 @@ public class UserController {
 
     // FUNCTIONS TO ADD MANAGER AND CLERK
     @PostMapping("/adminpanel/addManager")
-    public String addManager(@RequestParam String name, @RequestParam String contact, 
+    public String addManager(@RequestParam String name, @RequestParam long contact, 
                              @RequestParam String email, @RequestParam String password, 
                              @RequestParam String address) {
         User user = new User();
@@ -108,7 +117,7 @@ public class UserController {
     }
 
     @PostMapping("/adminpanel/addClerk")
-    public String addClerk(@RequestParam String name, @RequestParam String contact, 
+    public String addClerk(@RequestParam String name, @RequestParam long contact, 
                            @RequestParam String email, @RequestParam String password, 
                            @RequestParam String address) {
         User user = new User();
